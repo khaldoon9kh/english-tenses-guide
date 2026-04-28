06e5804e0229e67a45031fe14e27c9ae3dc9469b
KhaldoonKH <khaldoon9kh9@gmail.com>
2026-04-28 20:25:22 +0300

    add list of all verbs

--- src/data/irregularVerbs.js
+++ src/data/irregularVerbs.js
@@ -1,12 +1,64 @@
 export const IRREGULAR_VERBS = [
-  ["be", "was/were", "been"], ["go", "went", "gone"], ["eat", "ate", "eaten"],
-  ["have", "had", "had"], ["do", "did", "done"], ["take", "took", "taken"],
-  ["come", "came", "come"], ["see", "saw", "seen"], ["get", "got", "gotten"],
-  ["give", "gave", "given"], ["know", "knew", "known"], ["think", "thought", "thought"],
-  ["say", "said", "said"], ["make", "made", "made"], ["find", "found", "found"],
-  ["write", "wrote", "written"], ["read", "read", "read"], ["buy", "bought", "bought"],
-  ["bring", "brought", "brought"], ["feel", "felt", "felt"], ["leave", "left", "left"],
-  ["tell", "told", "told"], ["build", "built", "built"], ["sleep", "slept", "slept"],
-  ["meet", "met", "met"], ["speak", "spoke", "spoken"], ["begin", "began", "begun"],
-  ["drive", "drove", "driven"], ["send", "sent", "sent"], ["break", "broke", "broken"],
+  ["arise", "arose", "arisen"], ["awake", "awoke", "awoken"],
+  ["be", "was/were", "been"], ["bear", "bore", "borne"], ["beat", "beat", "beaten"],
+  ["become", "became", "become"], ["begin", "began", "begun"], ["bend", "bent", "bent"],
+  ["bet", "bet", "bet"], ["bind", "bound", "bound"], ["bite", "bit", "bitten"],
+  ["bleed", "bled", "bled"], ["blow", "blew", "blown"], ["break", "broke", "broken"],
+  ["breed", "bred", "bred"], ["bring", "brought", "brought"], ["broadcast", "broadcast", "broadcast"],
+  ["build", "built", "built"], ["burn", "burnt/burned", "burnt/burned"], ["burst", "burst", "burst"],
+  ["buy", "bought", "bought"],
+
+  ["catch", "caught", "caught"], ["choose", "chose", "chosen"], ["cling", "clung", "clung"],
+  ["come", "came", "come"], ["cost", "cost", "cost"], ["creep", "crept", "crept"], ["cut", "cut", "cut"],
+
+  ["deal", "dealt", "dealt"], ["dig", "dug", "dug"], ["do", "did", "done"], ["draw", "drew", "drawn"],
+  ["dream", "dreamt/dreamed", "dreamt/dreamed"], ["drink", "drank", "drunk"], ["drive", "drove", "driven"],
+
+  ["eat", "ate", "eaten"],
+
+  ["fall", "fell", "fallen"], ["feed", "fed", "fed"], ["feel", "felt", "felt"],
+  ["fight", "fought", "fought"], ["find", "found", "found"], ["flee", "fled", "fled"],
+  ["fly", "flew", "flown"], ["forbid", "forbade", "forbidden"], ["forget", "forgot", "forgotten"],
+  ["forgive", "forgave", "forgiven"], ["freeze", "froze", "frozen"],
+
+  ["get", "got", "got/gotten"], ["give", "gave", "given"], ["go", "went", "gone"],
+  ["grow", "grew", "grown"],
+
+  ["hang", "hung", "hung"], ["have", "had", "had"], ["hear", "heard", "heard"],
+  ["hide", "hid", "hidden"], ["hit", "hit", "hit"], ["hold", "held", "held"],
+  ["hurt", "hurt", "hurt"],
+
+  ["keep", "kept", "kept"], ["kneel", "knelt/kneeled", "knelt/kneeled"], ["know", "knew", "known"],
+
+  ["lay", "laid", "laid"], ["lead", "led", "led"], ["lean", "leant/leaned", "leant/leaned"],
+  ["leap", "leapt/leaped", "leapt/leaped"], ["learn", "learnt/learned", "learnt/learned"],
+  ["leave", "left", "left"], ["lend", "lent", "lent"], ["let", "let", "let"],
+  ["lie", "lay", "lain"], ["light", "lit/lighted", "lit/lighted"], ["lose", "lost", "lost"],
+
+  ["make", "made", "made"], ["mean", "meant", "meant"], ["meet", "met", "met"],
+  ["mistake", "mistook", "mistaken"],
+
+  ["pay", "paid", "paid"], ["put", "put", "put"],
+
+  ["read", "read", "read"], ["ride", "rode", "ridden"], ["ring", "rang", "rung"],
+  ["rise", "rose", "risen"], ["run", "ran", "run"],
+
+  ["say", "said", "said"], ["see", "saw", "seen"], ["seek", "sought", "sought"],
+  ["sell", "sold", "sold"], ["send", "sent", "sent"], ["set", "set", "set"],
+  ["shake", "shook", "shaken"], ["shine", "shone/shined", "shone/shined"],
+  ["shoot", "shot", "shot"], ["show", "showed", "shown"], ["shrink", "shrank", "shrunk"],
+  ["shut", "shut", "shut"], ["sing", "sang", "sung"], ["sink", "sank", "sunk"],
+  ["sit", "sat", "sat"], ["sleep", "slept", "slept"], ["slide", "slid", "slid"],
+  ["speak", "spoke", "spoken"], ["spend", "spent", "spent"], ["spill", "spilt/spilled", "spilt/spilled"],
+  ["spin", "spun", "spun"], ["split", "split", "split"], ["spread", "spread", "spread"],
+  ["stand", "stood", "stood"], ["steal", "stole", "stolen"], ["stick", "stuck", "stuck"],
+  ["sting", "stung", "stung"], ["strike", "struck", "struck"], ["swear", "swore", "sworn"],
+  ["sweep", "swept", "swept"], ["swim", "swam", "swum"], ["swing", "swung", "swung"],
+
+  ["take", "took", "taken"], ["teach", "taught", "taught"], ["tear", "tore", "torn"],
+  ["tell", "told", "told"], ["think", "thought", "thought"], ["throw", "threw", "thrown"],
+  ["understand", "understood", "understood"],
+
+  ["wake", "woke", "woken"], ["wear", "wore", "worn"], ["weep", "wept", "wept"],
+  ["win", "won", "won"], ["wind", "wound", "wound"], ["write", "wrote", "written"]
 ];
